d0b43c082dbc95e7dc657204885fb3aad65e122c
vexlexroy <leoroysab369@gmail.com>
2023-11-11 15:40:24 +0100

    Komponente za prvu predaju + rutiranje

--- Izvorni kod/my-app/src/App.js
+++ Izvorni kod/my-app/src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
+//node modules import
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
 import React from 'react';
+//css imports
+import './styles/App.css';
+//component imports
 import Login from './Components/Login';
+import SignUp from './Components/SignUp';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div>
-      <Header/>
-
-      <Login/>
-
-      <Footer/>
-    </div>
+    <Router>
+      <Routes>
+          <Route exact path='/' element={<div><Header/><Home/><Footer/></div>}></Route>
+          <Route exact path='/Login' element={<div><Header/><Login/><Footer/></div>}></Route>
+          <Route exact path='/SignUp' element={<div><Header/><SignUp/><Footer/></div>}></Route>
+      </Routes>
+    </Router> 
   );
 }
 
---
+++ Izvorni kod/my-app/src/Components/Home.js
@@ -0,0 +1,14 @@
+//node modules imports
+import React from "react";
+//css imports
+import '../styles/Home.css';
+
+function Home(){
+    return(
+        <div className = "home">
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+export default Home;
