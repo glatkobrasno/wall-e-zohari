2ee9e9e5557eacc22a795e01accb52ad04d458a0
vexlexroy <leoroysab369@gmail.com>
2023-11-12 14:47:11 +0100

    Header i Menue napravljeni

--- Izvorni kod/my-app/src/Components/Header.js
+++ Izvorni kod/my-app/src/Components/Header.js
@@ -1,19 +1,37 @@
 //modules imports
-import React from 'react';
+import React, { useState } from 'react';
 //css imports
 import '../styles/Header.css';
 //component imports
 import Menue from './Menue';
+import BackgroindImg1 from '../images/menueIcon_cloase.png'
 
 function Header(){
+    //variables
+    const [menueOpen, setMenueOpen] = useState(false);
+    var iconStyle = { transition: '0.3s'}
+    function menueIconClick(){
+        setMenueOpen(!menueOpen);
+    }
+
+    if(menueOpen){// changes the stile depending on menue state
+           iconStyle = {...iconStyle, backgroundColor : 'rgb(60, 86, 119)', backgroundImage : `url(${BackgroindImg1})`,}; 
+        }
+        else{ iconStyle = {...iconStyle,};
+        }
+
+    // render resault
     return(
         <div className = "header">
-            <div className = "menue_button"></div>
-            
-            <Menue/>
-        </div>
-        
 
+            <div className = "menue_button" 
+            style={iconStyle}
+            onClick={menueIconClick}
+            >
+            </div>
+            
+            {menueOpen && <Menue/>} {/*Adds menue if Icon is clicked*/}
+        </div>
     );
 }
 
--- Izvorni kod/my-app/src/Components/Menue.js
+++ Izvorni kod/my-app/src/Components/Menue.js
@@ -1,16 +1,19 @@
+//modules imports
 import React from "react";
 import { Link } from 'react-router-dom';
+//css imports
 import '../styles/Menue.css'
 
 function Menue(){
     return(
-        <div className="menue">
-            <ul className="select">
-                <Link to="/Login" ><li>LogIn</li></Link>
-                <li>nesto</li>
-                <li>nesto</li>
-            </ul>
-        </div>
+        
+            <div className="menue">
+                <ul className="select">
+                    <Link to="/Login"><li>LogIn</li></Link>
+                    <li>nesto</li>
+                    <li>nesto</li>
+                </ul>
+            </div>
     );
 }
 
