d894ad6e4dc45e741f3a10c14d47a2d5fb50a993
glatkobrasno <renato.brasnic@gmail.com>
2024-01-17 10:27:49 +0100

    Dodaj modul za security.
    TODO: parametri za vezu s bazom i ime tablice s lozinkama-

---
+++ IzvorniKod/backend/KuhajIT/security/security.py
@@ -0,0 +1,89 @@
+from django.db.models.base import connection
+import bcrypt
+import psycopg2
+from psycopg2 import sql
+# TODO: target tablica s korisnicima
+def connect_to_database():
+    # TODO: Ažuriraj spajanje
+    parametri = {
+        "dbname": "database_name",
+        "user": "username",
+        "password": "password",
+        "host": "host",
+        "port": "port"
+    }
+
+    try:
+        connection = psycopg2.connect(**parametri)
+        return connetcion
+    except psycopg2.Error as e:
+        print("Nisam se spojio s bazom.")
+        print(e)
+        return None
+
+def hash_password(password):
+    salt = bcrypt.gensalt() #  nasumični salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password, salt
+
+def store_password_in_database(username, hashed_password, salt):
+    # Spoji se
+    connection = connect_to_database()
+    if connection is None:
+        return
+
+    try:
+        # Spremi usera, lozinku i salt u bazu
+        with connection.cursor() as cursor:
+            insert_query = sql.SQL("INSERT INTO ??? (username, password_hash, salt) VALUES (%s, %s, %s)")
+            cursor.execute(insert_query, (username, hashed_password, salt))
+        connection.commit()
+        print("Lozinka spremljena u bazu.")
+    except psycopg2.Error as e:
+        print("Lozinka nije spremljena u bazu radi greške.")
+        print(e)
+    finally:
+        connection.close()
+
+def check_password_in_database(username, input_password):
+    # Spoji se na bazu
+    connection = connect_to_database()
+    if connection is None:
+        return False
+
+    try:
+        # Uzmi hashanu lozinku, usera i salt
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT password_hash, salt FROM ??? WHERE username = %s", (username,))
+            result = cursor.fetchone()
+
+            if result:
+                hashed_password_from_db, salt = result
+                # Hashaj input
+                hashed_input_password = bcrypt.hashpw(input_password.encode('utf-8'), salt)
+                # Usporedi dva hasha
+                return hashed_password_from_db == hashed_input_password
+            else:
+                # if not result -> user ne postoji u bazi
+                return False
+    except psycopg2.Error as e:
+        print("Greška u provjeravanju lozinke u bazi.")
+        print(e)
+        return False
+    finally:
+        connection.close()
+
+# ako je datoteka pozvana izravno
+if "__name__"=="__main__":
+    username = "ghostbusters"
+    input_password = "kogadazovemo"
+
+
+    hashed_password, salt = hash_password(input_password)
+    store_password_in_database(username, hashed_password, salt)
+
+    if check_password_in_database(username, input_password):
+        print("Lozinka je točna.")
+    else:
+        print("Lozinka nije točna.")
